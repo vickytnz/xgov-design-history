55b8e1743b5de06dfcff70caa6d19f64d696ba89
Vicky Teinaki <vicky.teinaki@gmail.com>
2024-07-09 21:25:47 +0100

    Initial commit

---
+++ eleventy.config.js
@@ -0,0 +1,37 @@
+module.exports = function (eleventyConfig) {
+  // Options to customise the appearance of your design history
+  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
+  eleventyConfig.addPlugin(require('@x-govuk/govuk-eleventy-plugin'), {
+    stylesheets: [
+      '/styles/application.css'
+    ],
+    headingPermalinks: true,
+    header: {
+      logotype: false,
+      productName: 'Design history',
+      search: {
+        indexPath: '/search.json',
+        sitemapPath: '/sitemap'
+      }
+    },
+    url:
+      process.env.GITHUB_ACTIONS &&
+      'https://x-govuk.github.io/govuk-design-history-template/'
+  })
+
+  // Passthrough
+  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
+
+  // Config
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    dir: {
+      input: 'app',
+      layouts: '_layouts',
+      includes: '_components'
+    },
+    pathPrefix: process.env.GITHUB_ACTIONS && '/govuk-design-history-template/'
+  }
+}
